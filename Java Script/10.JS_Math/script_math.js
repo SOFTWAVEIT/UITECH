c7480776d538c478143ffd0229d6f328a2f276a1
suniledureka <sunil.edureka@gmail.com>
2020-09-11 19:03:10 +0530

    JS

---
+++ Java Script/10.JS_Math/script_math.js
@@ -0,0 +1,24 @@
+console.log(`PI = ${Math.PI}`);
+console.log(`E = ${Math.E}`);
+
+console.log(`abs(-5) = ${Math.abs(-5)}`); //5
+console.log(`ceil(10.0) = ${Math.ceil(10.9)}`); //11
+console.log(`floor(10.9) = ${Math.floor(10.9)}`); //10
+
+console.log(`min(30,23,67) = ${Math.min(30,23,67)}`);
+console.log(`max(30,23,67) = ${Math.max(30,23,67)}`);
+
+console.log('pow(5,3) = '+ Math.pow(5,3));
+console.log(`pow(5,3) = ${5 ** 3}`) //ES6
+
+let rand = Math.random();
+console.log(`rand() = ${rand}`);
+otp = rand*10000;
+console.log(`OTP = ${Math.floor(otp)}`);
+
+console.log(`round() = ${Math.round(123.556)}`);
+
+console.log(`sqrt(25) = ${Math.sqrt(25)}`);
+
+console.log(`sin(0) = ${Math.sin(0)}`);
+console.log(`cos(0) = ${Math.cos(0)}`);
